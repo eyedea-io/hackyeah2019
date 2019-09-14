59a8737ad0c761f1e2bc33e0e53f7a637a734c40
Nelias <michal.antropik@gmail.com>
2019-09-15 00:43:28 +0200

    feat(main): make player go through all fields on the board

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 "use strict"
 
-import aframe from 'aframe'
-import animate from './animation'
+import aframe from "aframe"
+import animate from "./animation"
 
 // service worker registration - remove if you're not going to use it
 
@@ -26,26 +26,54 @@ if ("serviceWorker" in navigator) {
 // place your code below
 
 const board = [
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 },
-  { x: 0, y: 1 }
+  { position: "-3.5 0.4 4.5" },
+  { position: "-3.5 0.4 3.5" },
+  { position: "-3.5 0.4 2.5" },
+  { position: "-3.5 0.4 1.5" },
+  { position: "-3.5 0.4 0.5" },
+  { position: "-3.5 0.4 -0.5" },
+  { position: "-3.5 0.4 -1.5" },
+  { position: "-3.5 0.4 -2.5" },
+  { position: "-3.5 0.4 -3.5", turn: "right" },
+  { position: "-2.5 0.4 -3.5" },
+  { position: "-1.5 0.4 -3.5" },
+  { position: "-0.5 0.4 -3.5" },
+  { position: "0.5 0.4 -3.5", turn: "right" },
+  { position: "0.5 0.4 -2.5" },
+  { position: "0.5 0.4 -1.5", turn: "right" },
+  { position: "-.5 0.4 -1.5" },
+  { position: "-1.5 0.4 -1.5", turn: "right" },
+  { position: "-1.5 0.4 -0.5" },
+  { position: "-1.5 0.4 .5" },
+  { position: "-1.5 0.4 1.5", turn: "left" },
+  { position: "-.5 0.4 1.5" },
+  { position: ".5 0.4 1.5", turn: "right" },
+  { position: ".5 0.4 2.5" },
+  { position: ".5 0.4 3.5", turn: "left" },
+  { position: "1.5 0.4 3.5" },
+  { position: "2.5 0.4 3.5" },
+  { position: "3.5 0.4 3.5" },
+  { position: "3.5 0.4 2.5", turn: "left" },
+  { position: "3.5 0.4 1.5" },
+  { position: "3.5 0.4 0.5" },
+  { position: "3.5 0.4 -0.5" },
+  { position: "2.5 0.4 -0.5", turn: "left" },
+  { position: "1.5 0.4 -.5" },
+  { position: "1.5 0.4 -1.5" },
+  { position: "1.5 0.4 -2.5" },
+  { position: "2.5 0.4 -2.5" },
+  { position: "3.5 0.4 -2.5" },
+  { position: "3.5 0.4 -3.5" },
+  { position: "3.5 0.4 -4.5" },
+  { position: "0 0.4 10" }
 ]
 
-let gameStarted = false
+let gameStarted = true
 
 const game = () => {
+  board.forEach((elem, index) => {
+    setTimeout(() => animate(".player", elem.position), 500 * index)
+  })
   return 0
 }
 
